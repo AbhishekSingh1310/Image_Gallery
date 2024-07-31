6ea6a7963b70ae7fde5f556a974b2d3ed28addf5
Abhishek Singh <147223566+AbhishekSingh1310@users.noreply.github.com>
2024-07-31 19:52:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const changeBackgroundButton1 = document.getElementById('hero1');
+changeBackgroundButton1.addEventListener('click', function () {
+    document.body.style.backgroundImage = `url(https://wallpapercave.com/uwp/uwp3548126.jpeg)`;
+});
+const changeBackgroundButton2 = document.getElementById('hero2');
+changeBackgroundButton2.addEventListener('click', function () {
+    document.body.style.backgroundImage = `url(https://wallpapercave.com/wp/wp7346847.jpg)`;
+});
+const changeBackgroundButton3 = document.getElementById('hero3');
+changeBackgroundButton3.addEventListener('click', function () {
+    document.body.style.backgroundImage = `url(https://wallpapercave.com/wp/wp5024350.jpg)`;
+});
+const changeBackgroundButton4 = document.getElementById('hero4');
+changeBackgroundButton4.addEventListener('click', function () {
+    document.body.style.backgroundImage = `url(https://wallpapercave.com/wp/wp4584569.png)`;
+});
+const changeBackgroundButton5 = document.getElementById('hero5');
+changeBackgroundButton5.addEventListener('click', function () {
+    document.body.style.backgroundImage = `url(https://wallpapercave.com/wp/wp7725308.jpg)`;
+});
+const changeBackgroundButton6 = document.getElementById('hero6');
+changeBackgroundButton6.addEventListener('click', function () {
+    document.body.style.backgroundImage = `url(https://wallpapercave.com/wp/wp5390955.jpg)`;
+});
+const changeBackgroundButton7 = document.getElementById('hero7');
+changeBackgroundButton7.addEventListener('click', function () {
+    document.body.style.backgroundImage = `url(https://wallpapercave.com/wp/wp9186389.jpg)`;
+});
+const changeBackgroundButton8 = document.getElementById('hero8');
+changeBackgroundButton8.addEventListener('click', function () {
+    document.body.style.backgroundImage = `url(https://wallpapercg.com/media/ts_orig/12347.webp)`;
+});
